5610086b8d299ed8b376cae3a6e14aa223e88653
Sonabhargav <sonap_87@yahoo.com>
2022-03-31 19:00:21 -0500

    modified

---
+++ breeze.java
@@ -0,0 +1,25 @@
+package javaproject;
+
+public class breeze {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int a= 10;
+	int b=20;
+	int c= a+b;
+	System.out.println(c);
+	
+	int r=100;
+	int p=200;
+	int d=300;
+	System.out.println(r+p+d);
+	
+	
+	
+			
+		
+		
+	}
+
+}
